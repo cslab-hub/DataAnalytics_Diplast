6e1a0f625dbd4ff8fd70bf3be8e0057b7cff7987
StefanBloemheuvel <stefanbloemheuvel@gmail.com>
2022-05-17 14:21:40 +0200

    test

--- datatypeconverter.py
+++ datatypeconverter.py
@@ -2,31 +2,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# data = pd.read_parquet(r"C:\Users\20191577\Downloads\data_processed.parquet")
-data = pd.read_parquet(r"C:\Users\20191577\Downloads\dataset.parquet")
+data = pd.read_parquet(r"C:\Users\20191577\Downloads\data_processed.parquet")
 data = data.iloc[1,:]
-# of_interest = ['TIMESERIES_Druck_links[N/mm2]',
-# 'TIMESERIES_Druck_rechts[N/mm2]',
-# 'TIMESERIES_Kraft_links[N]',
-# 'TIMESERIES_Kraft_rechts[N]',
-# 'TIMESERIES_Thermocouple_WS[°C]',
-# 'TIMESERIES_Timestamp[s]',
-# 'TIMESERIES_Waermestromsensor[µV]',
-# 'TIMESERIES_Weg_links[mm]',
-# 'TIMESERIES_Weg_rechts[mm]',]
+of_interest = ['TIMESERIES_Druck_links[N/mm2]',
+'TIMESERIES_Druck_rechts[N/mm2]',
+'TIMESERIES_Kraft_links[N]',
+'TIMESERIES_Kraft_rechts[N]',
+'TIMESERIES_Thermocouple_WS[°C]',
+'TIMESERIES_Timestamp[s]',
+'TIMESERIES_Waermestromsensor[µV]',
+'TIMESERIES_Weg_links[mm]',
+'TIMESERIES_Weg_rechts[mm]',]
 
-# data = data[of_interest]
-# data = data.apply(pd.Series).T
+data = data[of_interest]
+data = data.apply(pd.Series).T
+data.columns = ['print_left', 'pressure_right', 'force_left' ,'force_right' ,'Thermocouple_WS' ,'timestamp' ,'heat flow' 'sensor' ,'way_left' ,'way_right']
+data = data.drop(columns=['timestamp'])
 
-
+data.insert(0,'TIME', pd.date_range(start=pd.Timestamp('10:10:15'), periods=data.shape[0], freq='1s'))
+data.to_csv('data/plastic_welding.csv', index=False)
+data
 
 
 # plt.plot(data.iloc[:,7])
 # %%
-
-
-
-
-# other dataset
-
-data = pd.read_parquet(r"C:\Users\20191577\Downloads\dataset.parquet")
+data

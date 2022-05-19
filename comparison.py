a6b7fd1f71e56456ffb5780af356c116d25d195c
JvdHoogen <57524082+JvdHoogen@users.noreply.github.com>
2022-05-19 15:16:05 +0200

    test

--- comparison.py
+++ comparison.py
@@ -40,7 +40,7 @@ data = data_loader()
 
 # print(data)
 def return_comparison():
-    st.title('Compare two time series')
+    st.title('Compare two time series!')
     st.markdown("""
         To check what happened within every individual time serie and to compare them with each other, we can plot them on this tab and see what happened throughout the process.
 

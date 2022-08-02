2f125be657d06c6153de3a327f8c8ca705123e75
JvdHoogen <57524082+JvdHoogen@users.noreply.github.com>
2022-08-02 15:18:54 +0200

    Updated installation and main/home

--- home.py
+++ home.py
@@ -9,24 +9,47 @@ def return_homepage():
         # image = Image.open('images/logo.jpeg')
         # st.image(image, use_column_width=True)
 
+
+
+
+    
     st.header('Welcome to the Data Analytics Tool')
+    
+    st.markdown(""" 
+        ##### This dashboard is developed by Jheronimus Academy of Data Science (JADS) and Osnabrück University (UOS). It is developed for the Di-Plast project. More information on the Di-Plast project can be found [here](https://www.nweurope.eu/projects/project-search/di-plast-digital-circular-economy-for-the-plastics-industry/). For information on this dedicated tool, we advise reading our [wiki](https://di-plast.sis.cs.uos.de/Wiki.jsp?page=Data%20Analytics) page containing all the necessary information for installation and interpretation.
+
+
+        """)
+
     st.markdown(
         """
-        #### This dashboard is developed by the Jheronimus Academy of Data Science (JADS) and Osnabrück University (UOS). More information on the Di-Plast project can be found [here](https://www.nweurope.eu/projects/project-search/di-plast-digital-circular-economy-for-the-plastics-industry/). For information on this dedicated tool, we advise reading our [wiki](https://di-plast.sis.cs.uos.de/Wiki.jsp?page=Data%20Analytics) page containing all the necessary information for installation and interpretation.
 
+     
+
+
+
+
+
+
+
+
+
+
+
+
+        """)
+
+
+
+    st.markdown(
+        """
         ##### The Data Analytics tool consists of several modules that analyse parts of your dataset. It is of great importance that the data used in this tool is properly validated. For validating the data, we advise you to check our data validation tool that can be accessed [here](https://share.streamlit.io/cslab-hub/data_validation_diplast/main/main.py).
-
-        #### **👈 Select a tool from the dropdown menu on the left**""")
+        ##### **👈 Select a tool from the dropdown menu on the left.**""")
     
 
-    # st.markdown(
-    #     """
-    #     ### Usage of the Data Analytics tool:\n
-    #     #### As can be seen on the left side of this page, several different applications can be accessed for analysing your data. However, to actually analyse the data, a datafile should be placed within the "data" folder of this package. 
 
 
-
-    #     """)
+  
 
     st.markdown(
         """
@@ -59,6 +82,22 @@ def return_homepage():
                 INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), 
                 EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
                 """)
+    st.markdown(""" Developed in the Di-Plast project in collaboration with:
+
+
+        """)
+
+    col1, col2  = st.columns(2)
+
+    with col1:
+        st.image('images/JADS_logo.png', width=500)
+
+
+    with col2:
+        st.image('images/Universitat_Osnabruck.png', width=400)
+
+
+
 
     st.markdown("""
         <div>Icons made by <a href="https://www.flaticon.com/authors/roundicons-premium" title="Roundicons Premium">Roundicons Premium</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- main.py
+++ main.py
@@ -4,32 +4,31 @@ st.set_page_config(page_title="Di-Plast Data Analytics Tool ",layout="wide")
 import pandas as pd
 import numpy as np
 from PIL import Image 
-
 from home import *
-# from matrix_profile import *
 from comparison import *
 from data_inspection import *
-# from forecast import *
 from ts_classifier import *
 from final_report import *
 from feature_selection import *
 # from EPA import *
 import base64
-    
-# st.sidebar.markdown(Logo_html, unsafe_allow_html=True)
+
+
+
 st.sidebar.image('images/di-plast-logo.png', use_column_width=True)
+
 st.sidebar.title("Select a Module")
-# st.sidebar.header("Each tool performs a different task.")
-
-
-
-
+    
 
 add_selectbox = st.sidebar.radio(
     "Choose one of the analytics options:",
-    ("Home",'Data Inspection','Data Comparison',"Feature Selection","Classifier","Final Data Report"),format_func= lambda x: 'Home' if x == 'Home' else f"{x}",help="Please select one of the options that aligns with your analytics needs."
+    ("Home",'Data Inspection','Data Comparison',"Feature Selection","Classifier","Exploratory Pattern Analytics","Final Data Report"),format_func= lambda x: 'Home' if x == 'Home' else f"{x}",help="Please select one of the options that aligns with your analytics needs."
     
-)         
+)               
+
+
+
+
 
 
 
@@ -37,11 +36,6 @@ add_selectbox = st.sidebar.radio(
 #! Home page
 if add_selectbox == 'Home':
     return_homepage()
-    
-    
-#! Page for the file format
-# if add_selectbox == 'Matrix Profile':
-#     return_matrix_profile()
 
 if add_selectbox == 'Data Comparison':
     return_comparison()
@@ -58,6 +52,11 @@ if add_selectbox == 'Feature Selection':
 if add_selectbox == 'Classifier':
     return_classifier()
 
+
+if add_selectbox == 'Exploratory Pattern Analytics':
+    return_EPA()
+
+
 if add_selectbox == 'Final Data Report':
     return_report()
     
@@ -72,6 +71,12 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 
-st.sidebar.image('images/JADS_logo_RGB.png', use_column_width=True)
+
+st.sidebar.markdown("***")
+
+st.sidebar.image('images/JADS_logo.png', use_column_width=True)
+
 
 st.sidebar.caption("[Bug reports and suggestions welcome ](mailto:j.o.d.hoogen@jads.nl)")
+
+

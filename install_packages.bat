e8619baa60fd5bd8b4ef8d8675c4b7b30a04198a
StefanBloemheuvel <stefanbloemheuvel@gmail.com>
2022-08-23 14:43:33 +0200

    changed removethis to data_analytics

--- install_packages.bat
+++ install_packages.bat
@@ -1,7 +1,7 @@
 echo "Started the process"
-call conda env remove --name removethis
-call conda create -n removethis -y
-call conda activate removethis
+call conda env remove --name data_analytics
+call conda create -n data_analytics -y
+call conda activate data_analytics
 call conda install pip -y
 call pip freeze
 call pip install -r packages.txt
